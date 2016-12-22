0dd6c03fc4347eac32fa2a80f73cfab3e2a16601
Morgan Sinclaire <sincm@uw.edu>
2016-12-21 18:07:01 -0800

    added 16.6 and 16.7

--- 16.py
+++ 16.py
@@ -28,6 +28,27 @@ def ttt_win(m):
 def trailing(n):
     return n / 5
 
+# 16.6
+# sorts each list, then simply compares the closest values in each list
+# runs in O(mlogm + nlogn + m + n) = O(mlogm + nlogn)
+def min_dif(a,b):
+    a,b = sorted(a),sorted(b)
+    m = abs(a[0] - b[0])
+    while a != [] and b != []:
+        d = abs(a[0] - b[0])
+        if d < m:
+            m = d
+        if a[0] < b[0]:
+            a = a[1:]
+        elif a[0] > b[0]:
+            b = b[1:]
+        else:
+            return 0
+    return m
+
+print(min_dif([1,3,15,11,2], [23,127,235,19,8]))
+
 # 16.7
+# this problem is dumb and hilarious
 def bigger(a,b):
-    pass
+    return (a,b)[str(a-b)[0] in ['-']]
