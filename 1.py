bdd00e1fce5f82763937c64921104203b52cccff
Morgan Sinclaire <sincm@uw.edu>
2016-12-22 15:38:01 -0800

    added another implementation of 1.4

--- 1.py
+++ 1.py
@@ -22,6 +22,7 @@ def URLify(s, n):
     return s
 
 # 1.4
+# runs in O(mn), where n is the string length and m is the number of unique letters
 def permpal(s):
     t = set(s)
     n = 0
@@ -29,6 +30,22 @@ def permpal(s):
         if s.count(c) % 2 == 1:
             n += 1
     return n <= 1
+
+# alternatively, this runs in O(nlogn) instead
+def permpal(s):
+    a = sorted("".join(s.split()))
+    lengths = []
+    count = 1
+
+    for i in range(len(a) - 1):
+        if a[i] != a[i+1]:
+            lengths.append(count)
+            count = 1
+        else:
+            count += 1
+    lengths.append(count)
+
+    return sum([i % 2 for i in lengths]) <= 1
 
 print permpal("tacocat")
 print permpal("permpal")
