c73ad179a2d08f636f6d05f1ea576554c3fede82
Morgan Sinclaire <sincm@uw.edu>
2017-02-12 12:01:41 -0800

    cleaned up all solutions

--- ch1.py
+++ ch1.py
@@ -1,34 +1,56 @@
 # 1.1
-# with sets, O(n) is easy
 def is_unique(s):
-    return list(s) == list(set(s))
+    '''
+    with sets, O(n) is easy
+    '''
+    return len(list(s)) == len(set(s))
 
-# without sets or anything else, sort the list and go through it in O(nlogn)
+# print(is_unique("abcdef"))
+# print(is_unique("abcdea"))
+
 def is_unique(s):
+    '''
+    without sets or anything else, sort the list and go through it in O(nlogn)
+    '''
     s = sorted(s)
     for i in range(len(s) - 1):
         if s[i] == s[i+1]:
             return False
     return True
 
+# print(is_unique("abcdef"))
+# print(is_unique("abcdea"))
+
 # 1.2
-# sort both lists and check for equality in O(nlogn + mlogm)
 def is_perm(a,b):
+    '''
+    sort both lists and check for equality in O(nlogn + mlogm)
+    '''
     return sorted(a) == sorted(b)
 
+# print(is_perm("tommarvoloriddle", "iamlordvoldemort"))
+# print(is_perm("tom marvolo riddle", "i am lordVoldemort"))
+
 # 1.3
-def URLify(s, n):
+def urlify(s, n):
+    '''
+    iterates through string and replaces spaces
+    '''
     end = 0
-    for i in xrange(n):
+    for i in range(n):
         if s[i] != ' ':
             end = i
     s = s[:end+1]
     s = s.replace(' ', '%20')
     return s
 
+# print(urlify("Mr John Smith    ", 13))
+
 # 1.4
-# runs in O(mn), where n is the string length and m is the number of unique letters
 def permpal(s):
+    '''
+    runs in O(mn), where n is the string length and m is the number of unique letters
+    '''
     s = s.lower().replace(" ","")
     t = set(s)
     n = 0
@@ -40,8 +62,10 @@ def permpal(s):
 # print(permpal("Tact Coe"))
 # print(permpal("Tact Coa"))
 
-# alternatively, this runs in O(nlogn) instead
 def permpal(s):
+    '''
+    alternatively, this runs in O(nlogn) instead
+    '''
     s = s.lower().replace(" ","")
     a = sorted(s)
     lengths = []
@@ -61,8 +85,10 @@ def permpal(s):
 # print(permpal("Tact Coa"))
 
 # 1.5
-# tests the cases of replacements separately from insertions/deletions
 def one_away(a,b):
+    '''
+    tests the cases of replacements separately from insertions/deletions
+    '''
     d = len(a) - len(b)
     if d < 0:
         a,b = b,a
@@ -83,9 +109,17 @@ def one_away(a,b):
     else:
         return False
 
+# print(one_away("pale","ple"))
+# print(one_away("pales","pale"))
+# print(one_away("pale","bale"))
+# print(one_away("pale","bake"))
+
 # 1.6
-# reused the for-loop from my second solution of 1.4
 def compress(s):
+    '''
+    iterates through string, creating compressed list that becomes a string.
+    would have been simpler to create string directly, but runtime would be higher
+    '''
     lengths = []
     count = 1
 
@@ -104,21 +138,48 @@ def compress(s):
     else:
         return s
 
+# print(compress("aabcccccaaa"))
+
 # 1.7
-# for each entry in the upper left quadrant, swaps it with the 3 corresponding entries
-# could have also transposed and reflected but I thought of this first
 import numpy as np
 def rotate(A):
+    '''
+    for each entry in the upper left quadrant, swaps it with the 3 corresponding entries
+    '''
     n = A.shape[0]
-    for i in range((n+1) / 2):
-        for j in range(n / 2):
+    for i in range((n+1) // 2):
+        for j in range(n // 2):
             w,x,y,z = A[i,j], A[j,n-i-1], A[n-i-1,n-j-1], A[n-j-1,i]
             A[i,j], A[j,n-i-1], A[n-i-1,n-j-1], A[n-j-1,i] = z,w,x,y
 
     return A
 
+# m = np.array([range(x,x+3) for x in [1,4,7]])
+# print(rotate(m))
+# m = np.array([range(x,x+4) for x in [1,5,9,13]])
+# print(rotate(m))
+
+def rotate(A):
+    '''
+    transposes, then reflects across vertical middle
+    '''
+    A = A.T
+    n = A.shape[0]
+    for i in range(n):
+        for j in range(n // 2):
+            A[i,j],A[i,n-j-1] = A[i,n-j-1],A[i,j]
+    return A
+
+# m = np.array([range(x,x+3) for x in [1,4,7]])
+# print(rotate(m))
+# m = np.array([range(x,x+4) for x in [1,5,9,13]])
+# print(rotate(m))
+
 # 1.8
 def set_zero(A):
+    '''
+    marks rows and columns, then zeros them out
+    '''
     m,n = A.shape
     rows,cols = [],[]
     for i in range(m):
@@ -134,13 +195,26 @@ def set_zero(A):
 
     return A
 
+# m = np.array([range(x,x+3) for x in [1,4,7]])
+# m[1,1] = 0
+# print(set_zero(m))
+# m = np.array([range(x,x+4) for x in [1,5,9,13]])
+# m[1,1] = 0
+# m[3,1] = 0
+# print(set_zero(m))
+
 # 1.9
-# can't think of a way to use isSubstring effectively, but this just seemed
-# like the way I'd do it, running in O(n) where n is the length of each string
 def is_rotation(s1,s2):
-    if len(s1 != len(s2)):
+    '''
+    can't think of a way to use isSubstring effectively, but this just seemed
+    like the way I'd do it, running in O(n) where n is the length of each string
+    '''
+    if len(s1) != len(s2):
         return False
     for i in range(len(s1)):
         if s1 == s2[-i:] + s2[:-i]:
             return True
     return False
+
+# print(is_rotation("waterbottle", "erbottlewat"))
+# print(is_rotation("waterbottle", "erbattlewat"))
